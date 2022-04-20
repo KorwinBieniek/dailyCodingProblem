938decdaea3cca702ffd72bf45d9fdb518febb88
Korwin <korwin-bieniek@wp.pl>
2022-04-20 17:53:38 +0200

    Apple question: Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.

--- day_10.py
+++ day_10.py
@@ -1,5 +1,6 @@
 # Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 import heapq
+import threading
 from time import sleep, time
 
 # However we block other functions from executing because we are working on signle core
